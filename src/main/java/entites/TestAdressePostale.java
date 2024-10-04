5660092e5fb516926e932161b2cd8efbbb36cd28
Arnaud-C18 <clavier.a18@gmail.com>
2024-10-04 19:53:11 +0200

    Deuxieme commit

--- src/main/java/entites/TestAdressePostale.java
+++ src/main/java/entites/TestAdressePostale.java
@@ -2,16 +2,8 @@ package entites;
 
 public class TestAdressePostale {
     public static void main(String[] args) {
-        AdressePostale adresse1 = new AdressePostale();
-        adresse1.numeroRue = 2;
-        adresse1.libelleRue = "Avenue Jean Jaures";
-        adresse1.codePostal = 37000;
-        adresse1.ville = "Tours";
+        AdressePostale adresse1 = new AdressePostale(2, "Avenue Jean Jaures", 37000, "Tours");
 
-        AdressePostale adresse2 = new AdressePostale();
-        adresse2.numeroRue = 21;
-        adresse2.libelleRue = "Rue Saint-Etienne";
-        adresse2.codePostal = 33000;
-        adresse2.ville = "Bordeaux";
+        AdressePostale adresse2 = new AdressePostale(21, "Rue Saint-Etienne", 33000, "Bordeaux");
     }
 }
--- src/main/java/entites/TestPersonne.java
+++ src/main/java/entites/TestPersonne.java
@@ -4,23 +4,14 @@ import entites2.Personne;
 
 public class TestPersonne {
     public static void main(String[] args) {
-        Personne personne1 = new Personne();
-        personne1.nom = "Doe";
-        personne1.prenom = "Jhon";
-        personne1.adresse = new AdressePostale();
-        personne1.adresse.numeroRue = 2;
-        personne1.adresse.libelleRue = "Avenue Jean Jaures";
-        personne1.adresse.codePostal = 37000;
-        personne1.adresse.ville = "Tours";
 
-        Personne personne2 = new Personne();
-        personne2.nom = "Brassens";
-        personne2.prenom = "Georges";
-        personne2.adresse = new AdressePostale();
-        personne2.adresse.numeroRue = 1;
-        personne2.adresse.libelleRue = "Bd Camille Blanc";
-        personne2.adresse.codePostal = 34200;
-        personne2.adresse.ville = "Sète";
+        Personne personne1 = new Personne("Doe", "Jhon");
 
+        AdressePostale adresse = new AdressePostale(2,"rue de mont",37000,"Tours");
+        personne1.setAdresse(adresse);
+
+        personne1.AfficherNomPrenom();
+
+        String adresseVerifie = personne1.getAdresse().toString();
     }
 }
