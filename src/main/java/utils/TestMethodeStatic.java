28155b7d485833f7037363310b19375ced119e3d
Arnaud-C18 <clavier.a18@gmail.com>
2024-10-07 11:13:36 +0200

    Septieme commit

---
+++ src/main/java/utils/TestMethodeStatic.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class TestMethodeStatic {
+    public static void main(String[] args) {
+        String chaine = "12";
+        int nombre = Integer.parseInt(chaine);
+
+        int a = 10;
+        int b = 20;
+        int max = Math.max(a, b);
+
+        System.out.println(max);
+    }
+}
